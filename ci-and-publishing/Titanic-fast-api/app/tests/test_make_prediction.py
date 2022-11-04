0697f087e0f8ac34515455f932b60bed80d2bf1f
erick.calderin <edcm.erick@gmail.com>
2022-11-04 16:13:59 -0300

    port 8001

--- ci-and-publishing/Titanic-fast-api/app/tests/test_make_prediction.py
+++ ci-and-publishing/Titanic-fast-api/app/tests/test_make_prediction.py
@@ -5,7 +5,7 @@ from fastapi.testclient import TestClient
 
 def test_make_prediction(client: TestClient, load_data: pd.DataFrame) -> None:
     payload = {"inputs": load_data.replace({np.nan: None}).to_dict(orient="records")}
-    response = client.post(url="http://localhost:8000/api/v1/predict", json=payload)
+    response = client.post(url="http://localhost:8001/api/v1/predict", json=payload)
 
     assert response.status_code == 200
     response = response.json()
